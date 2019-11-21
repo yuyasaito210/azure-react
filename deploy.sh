dad1ab53e5f2a60254bc7227caf5c373e67b02dc
yuyasaito210 <yuyasaito210@gmail.com>
2019-11-21 15:53:34 -0800

    updated dir structure

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Stop previous dockers
+docker-compose stop
+docker-compose down
+
+# Run dockers as daemon
+docker-compose up -d --build
+
+# Remove caches
+# yes | docker container prune
+# yes | docker network prune
+# yes | docker image prune
